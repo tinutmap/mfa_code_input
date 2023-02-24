d147b475c7fb26b216c48c2e29ef362831a8f618
Tin Dinh <tinutmap@gmail.com>
2023-02-24 16:48:02 -0500

    Add timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [timer, setTimer] = React.useState(30)
+  useEffect(()=>{
+    let reduceTimer: NodeJS.Timer
+    if (timer > 0)    {
+      reduceTimer = setInterval(()=>setTimer(time => time - 1),1000)
+    }
+    return ()=> clearInterval(reduceTimer)
+  },[timer])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Timer {timer} second(s)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
