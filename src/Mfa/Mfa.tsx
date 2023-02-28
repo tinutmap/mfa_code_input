ec53b6096e9a383bbf6ee1ec97a2cab3cb5bb557
Tin Dinh <tinutmap@gmail.com>
2023-02-28 17:16:51 -0500

    add prettier

--- src/Mfa/Mfa.tsx
+++ src/Mfa/Mfa.tsx
@@ -1,17 +1,17 @@
-import React, { FC, ReactNode, useEffect, useRef, useState } from "react";
+import React, { FC, ReactNode, useEffect, useRef, useState } from 'react';
 
 const ALLOWED_KEY = Array.from({ length: 10 }, (_, i) => i.toString()).concat([
   // 'Backspace', 'Delete',
-  ''
-])
+  '',
+]);
 
 type MfaCodeDigitTileProps = {
-  index: number
-  digit: string
-  setCode: React.Dispatch<React.SetStateAction<string[]>>
-  setCurrentTileIndex: React.Dispatch<React.SetStateAction<number>>
-  isCurrentTile: boolean
-}
+  index: number;
+  digit: string;
+  setCode: React.Dispatch<React.SetStateAction<string[]>>;
+  setCurrentTileIndex: React.Dispatch<React.SetStateAction<number>>;
+  isCurrentTile: boolean;
+};
 
 const MfaCodeDigitTile: FC<MfaCodeDigitTileProps> = ({
   index,
@@ -22,34 +22,34 @@ const MfaCodeDigitTile: FC<MfaCodeDigitTileProps> = ({
 }) => {
   const updateCode = (value: string) => {
     if (ALLOWED_KEY.includes(value)) {
-      setCode(code => {
-        const newCode = code
-        newCode[index] = value
+      setCode((code) => {
+        const newCode = code;
+        newCode[index] = value;
         if (value !== '') {
-          setCurrentTileIndex(index + 1)
+          setCurrentTileIndex(index + 1);
         }
-        return [...newCode]
-      })
+        return [...newCode];
+      });
     }
-  }
+  };
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
-      return console.log('Enter pressed')
+      return console.log('Enter pressed');
     }
     if (event.key === 'Backspace' || event.key === 'Delete') {
-      if (digit) return
+      if (digit) return;
       if (!digit) {
-        event.preventDefault()
-        return setCurrentTileIndex(index - 1)
+        event.preventDefault();
+        return setCurrentTileIndex(index - 1);
       }
     }
-  }
-  const tileRef = useRef<HTMLInputElement>(null)
+  };
+  const tileRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
     if (tileRef.current && isCurrentTile) {
-      tileRef.current.focus()
+      tileRef.current.focus();
     }
-  }, [isCurrentTile])
+  }, [isCurrentTile]);
 
   return (
     <input
@@ -59,28 +59,23 @@ const MfaCodeDigitTile: FC<MfaCodeDigitTileProps> = ({
       placeholder={index.toString()}
       value={digit}
       onChange={(e) => updateCode(e.target.value)}
-      onKeyDown={event => handleKeyDown(event)}
+      onKeyDown={(event) => handleKeyDown(event)}
       ref={tileRef}
     />
-  )
-
-}
+  );
+};
 
 type MfaTilesProps = {
-  length: number
-}
-const MfaTiles: FC<MfaTilesProps> = ({
-  length,
-}) => {
-  const [code, setCode] = useState(
-    Array.from({ length: length }, (_,) => '')
-  )
+  length: number;
+};
+const MfaTiles: FC<MfaTilesProps> = ({ length }) => {
+  const [code, setCode] = useState(Array.from({ length: length }, (_) => ''));
 
-  const [currentTileIndex, setCurrentTileIndex] = useState(0)
+  const [currentTileIndex, setCurrentTileIndex] = useState(0);
 
   return (
     <>
-      {code.map((digit, index) =>
+      {code.map((digit, index) => (
         <MfaCodeDigitTile
           key={index}
           index={index}
@@ -89,30 +84,20 @@ const MfaTiles: FC<MfaTilesProps> = ({
           isCurrentTile={currentTileIndex === index}
           setCurrentTileIndex={setCurrentTileIndex}
         />
-      )}
+      ))}
     </>
-  )
-}
+  );
+};
 
 type MfaWrapperProps = {
-  children?: ReactNode
-}
+  children?: ReactNode;
+};
 
-export const MfaWrapper: FC<MfaWrapperProps> = ({
-  children
-}): JSX.Element => {
-  const isMfaAuthenticated = false
+export const MfaWrapper: FC<MfaWrapperProps> = ({ children }): JSX.Element => {
+  const isMfaAuthenticated = false;
 
   if (!isMfaAuthenticated) {
-    return (
-      <MfaTiles
-        length={6}
-      />
-    )
+    return <MfaTiles length={6} />;
   }
-  return (
-    <div>
-      {children}
-    </div>
-  )
-}
+  return <div>{children}</div>;
+};
