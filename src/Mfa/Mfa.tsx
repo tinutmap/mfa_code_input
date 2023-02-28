51a7dd24d674c8f276040828e9a2248b3cf928f2
Tin Dinh <tinutmap@gmail.com>
2023-02-27 21:42:15 -0500

    wip

--- src/Mfa/Mfa.tsx
+++ src/Mfa/Mfa.tsx
@@ -1,7 +1,82 @@
-import React, { FC } from "react";
+import React, { FC, ReactNode, useState } from "react";
+
+const ALLOWED_KEY = Array.from({length: 10}, (_,i)=> i.toString()).concat( [
+  // 'Backspace', 'Delete',
+  ''
+])
+
+type MfaCodeDigitTileProps = {
+  index: number
+  digit: string
+  setCode: React.Dispatch<React.SetStateAction<string[]>>
+
+}
+
+const MfaCodeDigitTile: FC<MfaCodeDigitTileProps> = ({
+  index,
+  digit,
+  setCode,
+}) => {
+  const updateCode = (value:string)=>{
+    if (ALLOWED_KEY.includes(value)) {
+      setCode(code => {
+        const newCode = code
+        newCode[index]=value
+        // console.log({newCode})
+        return [...newCode]
+      })
+    }
+  }
+
+  return (
+    <input 
+      type="text"
+      minLength={1}
+      maxLength={1}
+      // placeholder={index.toString()}
+      value={digit}
+      onChange={(e)=> updateCode(e.target.value)}
+      onKeyDown={(event)=> {
+        if (event.key === 'Enter') {
+          return console.log('Enter pressed')
+        }
+        if (event.key === 'Backspace' || event.key==='Delete') {
+          return
+        }
+      }}
+    />
+  )
+
+}
+
+type MfaTilesProps= {
+  length: number
+  // code: string
+}
+const MfaTiles: FC<MfaTilesProps> = ({
+  length,
+  // code,
+  // setCode
+}) =>{
+  const [code,setCode] = useState(
+    Array.from({length: length}, (_,i)=> 
+    // (i+1).toString()
+    ''
+    )
+    )
+
+  return (
+    <>
+      {code.map((digit,index) =>
+        // <span> {digit} </span>
+        <MfaCodeDigitTile key={index} index={index} digit={digit} setCode={setCode}/>
+      )}
+    </>
+  )
+}
 
 type MfaWrapperProps = {
-  children?: React.ReactNode
+  children?: ReactNode
 }
 
 export const MfaWrapper: FC<MfaWrapperProps> = ({
@@ -11,7 +86,11 @@ export const MfaWrapper: FC<MfaWrapperProps> = ({
 
   if (!isMfaAuthenticated) {
     return (
-      <div> MFA Tiles</div>
+      <MfaTiles
+        //  code={code}
+        // setCode={setCode}
+        length={6}
+      />
   )}
   return (
     <div>
