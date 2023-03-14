4f4450deef0168cf33d227628e21b4c8a263fe65
Tin Dinh <tinutmap@gmail.com>
2023-03-14 15:04:33 -0400

    fetch function using async/await

--- src/Mfa/Mfa.query.ts
+++ src/Mfa/Mfa.query.ts
@@ -11,19 +11,23 @@ export type MfaStatusDatatype = {
   mfaCodeLength: number;
 };
 
-export const getMfaStatus = (): Promise<MfaStatusDatatype> => {
-  return fetch('http://localhost:3000/mfa/status')
-    .then((res) => {
-      try {
-        if (res.ok) {
-          return res.json() as Promise<MfaStatusDatatype>;
-        }
-        throw new Error(res.status.toString());
-      } catch (err) {
-        throw new Error(err as string);
-      }
-    })
-    .catch((error) => {
-      throw new Error(error as string);
-    });
+export const getMfaStatus = async (): Promise<MfaStatusDatatype> => {
+  // return fetch('http://localhost:3000/mfa/status')
+  //   .then((res) => {
+  //     try {
+  //       if (res.ok) {
+  //         return res.json() as Promise<MfaStatusDatatype>;
+  //       }
+  //       throw new Error(res.status.toString());
+  //     } catch (err) {
+  //       throw new Error(err as string);
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     throw new Error(error as string);
+  //   });
+  const res = await fetch('http://localhost:3000/mfa/status');
+  if (res.ok) {
+    return res.json() as Promise<MfaStatusDatatype>;
+  } else throw new Error(`STATUS ${res.status}: ${res.statusText}`);
 };
