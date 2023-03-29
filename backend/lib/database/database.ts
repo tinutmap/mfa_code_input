79d7f107fa319c7e5c2556ef0c6022035d9ba364
Tin Dinh <tinutmap@gmail.com>
2023-03-29 14:31:16 -0400

    async/await createTable

--- backend/lib/database/database.ts
+++ backend/lib/database/database.ts
@@ -1,5 +1,5 @@
-import sqlite3 = require('sqlite3');
-import fs = require('fs');
+import sqlite3 from 'sqlite3';
+import fs from 'fs';
 
 const sqlite = sqlite3.verbose();
 const FILE_PATH = './backend/lib/database/database.db';
@@ -8,24 +8,32 @@ export function createDbConnection() {
   if (fs.existsSync(FILE_PATH)) {
     return new sqlite3.Database(FILE_PATH);
   } else {
-    const db = new sqlite.Database(FILE_PATH, (error) => {
+    const db = new sqlite.Database(FILE_PATH, async (error) => {
       if (error) {
         return console.error(error.message);
       }
-      createTable(db);
+      await createTable(db);
     });
     console.log('Connection with SQLite has been established');
     return db;
   }
 }
 
-function createTable(db: sqlite3.Database) {
-  db.exec(`
+async function createTable(db: sqlite3.Database) {
+  const sql = `
   CREATE TABLE MfaRecords
   (
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
     code VARCHAR(10) NOT NULL,
     created_time TEXT DEFAULT CURRENT_TIMESTAMP 
   );
-`);
+`;
+  return new Promise((resolve, reject) =>
+    db.exec(sql, (error) => {
+      if (error) {
+        return reject(console.log(error.message));
+      }
+      return resolve(console.log('Table created OK'));
+    })
+  );
 }
