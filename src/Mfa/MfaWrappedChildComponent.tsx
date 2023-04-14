f5dbf2995e2768a6d93a37c6c86797fa667e975b
Tin Dinh <tinutmap@gmail.com>
2023-04-14 17:00:39 -0400

    MVP

---
+++ src/Mfa/MfaWrappedChildComponent.tsx
@@ -0,0 +1,35 @@
+import { ResponseStatus } from '../lib/useAsync';
+import { MfaExpiredDateTimeDatatype, getMfaExpiredDateTime } from './Mfa.query';
+import { useAsync } from '../lib/useAsync';
+import React, { useState } from 'react';
+
+export const MfaWrappedChildComponent = () => {
+  const [doRefetch, setDoRefetch] = useState(false);
+  const { data, status } = useAsync<MfaExpiredDateTimeDatatype>(
+    getMfaExpiredDateTime,
+    [doRefetch]
+  );
+  console.log({ data, type: status });
+
+  switch (status) {
+    case ResponseStatus.Pending: {
+      return <p>Loading...</p>;
+    }
+    case ResponseStatus.Reject: {
+      throw new Error(data.toString());
+    }
+    case ResponseStatus.Resolved: {
+      const { mfaExpiredTime } = data;
+      return (
+        <div>
+          <p>{`MFA will expire in ${mfaExpiredTime}`}</p>
+          <input
+            type="button"
+            onClick={async () => setDoRefetch((state: boolean) => !state)}
+            value="Check MFA Expiration Time Again"
+          />
+        </div>
+      );
+    }
+  }
+};
