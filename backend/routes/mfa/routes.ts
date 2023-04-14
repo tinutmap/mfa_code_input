72760d621b62f85175528702b25e5ef1b06c370e
Tin Dinh <tinutmap@gmail.com>
2023-04-14 10:50:06 -0400

    MVP

---
+++ backend/routes/mfa/routes.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import { createMfaAuthenticationSessions, createMfaCode } from './set';
+import { checkMfaCode, getLastMfaCreatedTime, checkMfaStatus } from './get';
+import { TIMER_DURATION_IN_MILLISECOND } from './constants';
+
+const router = express.Router();
+
+router.get('/status', async (req, res) => {
+  const count = await checkMfaStatus();
+  if (count > 0) {
+    return res.send({ isMfaAuthenticated: true });
+  } else {
+    return res.send({ isMfaAuthenticated: false, mfaCodeLength: 6 });
+  }
+});
+
+router.post('/submit-mfa-code', async (req, res) => {
+  const { body } = req;
+  const isMfaCodeMatched = await checkMfaCode(body.mfaCode);
+
+  if (!isMfaCodeMatched) return res.sendStatus(401);
+
+  await createMfaAuthenticationSessions();
+  return res.sendStatus(200);
+});
+
+router.post('/send-code', async (req, res, next) => {
+  let lastMfaCodeCreatedTime = await getLastMfaCreatedTime();
+  if (lastMfaCodeCreatedTime) lastMfaCodeCreatedTime += ' UTC'; // NOTE: needed to append ' UTC' timezone here in order to parse correctly. SQLite does not carry timezone.
+  if (
+    !lastMfaCodeCreatedTime || // NOTE: this condition covers the case no MfaCode has ever generated.
+    Date.now() - Date.parse(lastMfaCodeCreatedTime) >
+      TIMER_DURATION_IN_MILLISECOND
+  ) {
+    const testMessageUrl = await createMfaCode();
+    // res.redirect(testMessageUrl as string);
+    return res
+      .status(200)
+      .send({ timerDurationInMillisecond: TIMER_DURATION_IN_MILLISECOND });
+  }
+  return res.sendStatus(401);
+});
+
+export default router;
